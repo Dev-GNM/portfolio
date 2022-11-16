dbb37d7c3b13747b9575ec00cc2a9cb981818d10
Geoffrey <geoffreynjihia15@gmail.com>
2022-11-16 19:50:49 +0300

    added tailwind screen breakpoints

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,19 +6,22 @@ export default function Navbar() {
     <section className='min-h-30 pt-5 text-center'>
       <h1 className='font-bold text-3xl'>Hi, I'm Geoffrey Njihia, a fullstack software developer...</h1>
     </section>
-  <div class="flex items-center justify-between pt-4">
+  <div className="flex items-center justify-between pt-4">
  
-    <div class="pt-2">
+    <div className="pt-2">
         <img src="images/mylogo3.PNG" alt=""/>
     </div>
   
-    <div class="hidden md:flex space-x-8">
+    <div className="hidden md:flex space-x-8 pl-2">
+    
         <a href="#" class="hover:text-darkGrayishBlue">Home</a>
         <a href="#"class="hover:text-darkGrayishBlue">Background</a>
         <a href="#"class="hover:text-darkGrayishBlue">Blog</a>
     </div>
- 
-  <a href="#" class="hidden md:block p-3 px-6 pt-2 text-black bg-brightRed rounded-full baseline hover:bg-brightRedLight"
+    <a href="#" class="hidden md:block p-3 pt-2 text-black bg-brightRed rounded-full baseline hover:bg-brightRedLight"
+  >Resume</a>
+
+  <a href="#" className="hidden md:block p-3 px-6 pt-2 mr-4 text-black bg-brightRed rounded-full baseline hover:bg-brightRedLight"
   >Contact Me</a>
   </div>
     </>
