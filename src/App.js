17f54b909d4d5b89a4945282cd35fbe768d267b4
Geoffrey <geoffreynjihia15@gmail.com>
2022-11-16 19:31:28 +0300

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
+import Navbar from "./components/Navbar";
 
 
 function App() {
   return (
     <>
-     <h1 className="text-3xl font-bold underline">Hi! I'm Geoffrey Njihia, a fullstack software developer...</h1>
+     <Navbar />
     </>
   );
 }
