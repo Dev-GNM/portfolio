44bf41a9efed6d2f3c80bba9840c1eaaaa648a31
Geoffrey <geoffreynjihia15@gmail.com>
2022-11-19 12:22:24 +0300

    added inset positioning on sidebar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,27 +3,31 @@ import React from 'react'
 export default function Navbar() {
   return (
     <>
-    <section className='min-h-30 pt-5 text-center text-white pb-6 bg-veryLightDark h-35'>
+    <section className='min-h-30 pt-8 text-center text-white pb-6 bg-gray-900 h-35 '>
       <h1 className='font-bold text-3xl'>Hi, I'm Geoffrey Njihia, a fullstack software developer...</h1>
-    </section>
-  <div className="flex items-center justify-between pt-4 pb-5 text-white bg-veryLightDark">
+   
+  <div className=" flex items-center justify-between pt-5 pb-5 text-white">
  
     <div className="pt-2">
         <img src="images/mylogo3.PNG" alt=""/>
     </div>
   
-    <div className="hidden md:flex space-x-8 pl-2">
+    <div className="hidden md:flex space-x-8 pl-2 pt-5">
     
         <a href="#" class="hover:text-darkGrayishBlue">Home</a>
-        <a href="#"class="hover:text-darkGrayishBlue">Background</a>
+        <a href="#"class="hover:text-darkGrayishBlue">Portfolio</a>
         <a href="#"class="hover:text-darkGrayishBlue">Articles</a>
     </div>
-    <a href="#" class="hidden md:block p-3 pt-2 text-white bg-brightRed rounded-full baseline hover:bg-brightRedLight"
+    <div className='flex ml-8'>
+    <a href="#" class="hidden md:block p-4 pt-4 text-white bg-brightRed rounded-full baseline hover:bg-brightRedLight"
   >Resume</a>
 
-  <a href="#" className="hidden md:block p-3 px-6 pt-2 mr-4 text-white bg-brightRed rounded-full baseline hover:bg-brightRedLight"
+  <a href="#" className="hidden md:block p-2 px-6 pt-4 mr-6 text-white bg-brightRed rounded-full baseline hover:bg-brightRedLight"
   >Contact Me</a>
+    </div>
+   
   </div>
+  </section>
     </>
   )
 }
